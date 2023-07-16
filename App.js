29e87a1427cf3d697b82f14081e3fb788d6432ec
Chenvenom89 <127424104+Chenvenom89@users.noreply.github.com>
2023-07-16 20:36:30 +0300

    Add files via upload
    Can’t get anything on the console.log

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Adminstrators from './components/Adminstrators';
+
+function App() {
+<Adminstrators></Adminstrators>
+  
+}
+
+export default App;
+
+
+
+//bring up react.(port3000 and connect to port 4000 where the )
+//const "any table" =  const flights = fetch (localhost:4000/get flights) use "useEffect"
+//search for C.O.R.S "npm install CORS"
